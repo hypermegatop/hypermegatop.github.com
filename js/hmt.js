ef96e8d9eb9701078d97ae7d74feb35dc3f2f8b3
Martin Angers <martin.n.angers@gmail.com>
2012-04-23 21:18:10 -0400

    mucho less+css cleanup, share styling

--- js/hmt.js
+++ js/hmt.js
@@ -1,4 +1,21 @@
 jQuery(function($) {
+	var $share = $(".share-initial"),
+		$h1 = $("h1");
+
 	$("pre").addClass("prettyprint linenums");
 	prettyPrint();
+
+	$h1.on("click", function() {
+		$share.removeClass("share-tease");
+		$share.addClass("share-final");
+		$h1.off("mouseenter mouseleave");
+		$h1.addClass("unclikable");
+	});
+	$h1.hover(function() {
+		if (!$share.hasClass("share-final")) {
+			$share.addClass("share-tease");
+		}
+	}, function() {
+		$share.removeClass("share-tease");
+	});
 });
